3263f263320e223fc8affe08b6ac112691759e63
Alex Rowley <arowley@gmail.com>
2026-06-26 19:42:35 +1000

    {chore}: release 0.3.20

--- ghosttrap_cli/cli.py
+++ ghosttrap_cli/cli.py
@@ -13,7 +13,7 @@ import urllib.request
 
 import websockets
 
-__version__ = "0.3.19"
+__version__ = "0.3.20"
 
 GHOSTTRAP_SERVER = "wss://ghosttrap.io/stream/"
 CONFIG_DIR = os.path.expanduser("~/.ghosttrap")
