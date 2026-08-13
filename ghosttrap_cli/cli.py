5f46f31c0eba7b69209bd9b27656d8cabf213f7b
Alex Rowley <arowley@gmail.com>
2026-08-13 14:35:53 +1000

    {chore}: release 0.3.34

--- ghosttrap_cli/cli.py
+++ ghosttrap_cli/cli.py
@@ -28,7 +28,7 @@ def _harden_signals():
         except (OSError, ValueError):
             pass
 
-__version__ = "0.3.33"
+__version__ = "0.3.34"
 
 GHOSTTRAP_SERVER = "wss://ghosttrap.io/stream/"
 CONFIG_DIR = os.path.expanduser("~/.ghosttrap")
